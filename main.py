76227636b52fd633e1d83e2bfad378e3428be779
APZOUILLE <antonin.pretet@viacesi.fr>
2025-06-27 18:33:23 +0200

    update read me

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.blur import process_video
 # Tu peux changer ici selon le type d'objet à flouter :
 # COCO classes : 0 = personne, 2 = voiture, 5 = bus, 7 = camion, etc.
 if __name__ == "__main__":
+    
     filename = 'extrait_villejourrapide'  # Nom du fichier vidéo sans extension
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
@@ -14,12 +15,12 @@ if __name__ == "__main__":
     #treshold = 0.15  # seuil de confiance pour les détections
     #param tracker
 
-    #model dispo
-    #yolov8n.pt très petit
-    #yolo11n.pt petit
-    #yolo11.pt moyen
-    #yolo11l.pt grand
-    #yolo11x.pt très grand
+    # Decommenter une ligne pour choisir un modèle
+    #model_name = "yolov8n.pt" #très petit
+    model_name = "yolo11n.pt" #petit
+    #model_name = "yolo11.pt" #moyen
+    #model_name = "yolo11l.pt" #grand
+    #model_name = "yolo11x.pt" #très grand
 
-    process_video(input_path, output_path, model_name="yolo11x.pt", class_ids=[0,2],
+    process_video(input_path, output_path, model_name=model_name, class_ids=[0,2],
                    tracker_type='byte', ksize=51)
